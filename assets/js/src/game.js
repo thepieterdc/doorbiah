2b630444629cabbea573b6728fd736bc3170c2c7
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-10-08 20:36:47 +0200

    Centered

--- assets/js/src/game.js
+++ assets/js/src/game.js
@@ -1,42 +1,53 @@
 const $scoreDisplay = $("#score-display");
 
-const Head = function(canvas) {
+const Head = function (canvas) {
 	const self = this;
 	
 	this.height = 30;
 	this.width = 24.9;
 	this.x = canvas.width / 2;
-	this.y = canvas.height /2;
+	this.y = canvas.height / 2;
 	this.loaded = false;
 	this.closed_image = null;
 	this.open_image = null;
 	this.mouth_status = false;
 	
-	this.getImage = function() {
+	this.dx = 0;
+	this.dy = 0;
+	
+	this.getImage = function () {
 		return self.mouth_status ? self.open_image : self.closed_image;
 	};
 	
-	this.load_images = function() {
+	this.load_images = function () {
 		self.closed_image = new Image();
 		self.closed_image.src = 'assets/images/tobiah_closed.png';
-		self.closed_image.onload = function() {
+		self.closed_image.onload = function () {
 			console.log("woehoew");
 			self.open_image = new Image();
 			self.open_image.src = 'assets/images/tobiah_open.png';
-			self.open_image.onload = function() {
+			self.open_image.onload = function () {
 				self.loaded = true;
 			}
 		}
 	};
 	
-	this.toggle_mouth = function(status) {
+	this.toggle_mouth = function (status) {
 		self.mouth_status = status;
+	};
+	
+	this.x_center = function () {
+		return self.x - self.width / 2;
+	};
+	
+	this.y_center = function () {
+		return self.y - self.height / 2;
 	};
 	
 	this.load_images();
 };
 
-const Game = function(canvas, ctx) {
+const Game = function (canvas, ctx) {
 	const self = this;
 	this.canvas = canvas;
 	this.ctx = ctx;
@@ -45,29 +56,33 @@ const Game = function(canvas, ctx) {
 	
 	this.head = new Head(canvas);
 	
-	this.drawHead = function() {
-		if(self.head.loaded) {
-			self.ctx.drawImage(self.head.getImage(), self.head.x, self.head.y, self.head.width, self.head.height);
+	this.drawHead = function () {
+		if (self.head.loaded) {
+			self.ctx.drawImage(self.head.getImage(), self.head.x_center(), self.head.y_center(), self.head.width, self.head.height);
 		}
 	};
 	
-	this.keyDownHandler = function(e) {
-		if(e.keyCode === 32) {
+	this.keyDownHandler = function (e) {
+		if (e.keyCode === 32) {
 			self.head.toggle_mouth(true);
 		}
 	};
 	
-	this.keyUpHandler = function(e) {
-		if(e.keyCode === 32) {
+	this.keyUpHandler = function (e) {
+		if (e.keyCode === 32) {
 			self.head.toggle_mouth(false);
 		}
 	};
 	
-	this.updateScore = function() {
+	this.moveHead = function () {
+	
+	};
+	
+	this.updateScore = function () {
 		$scoreDisplay.text(self.score);
 	};
 	
-	this.draw = function() {
+	this.draw = function () {
 		self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
 		
 		self.drawHead();
