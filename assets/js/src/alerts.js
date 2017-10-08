a483ec28656806ef0e60d53446f14d36d33c2a82
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-10-09 00:27:44 +0200

    Gameover done

--- assets/js/src/alerts.js
+++ assets/js/src/alerts.js
@@ -2,8 +2,14 @@ const $alertsBox = $('#alerts');
 const $alertsTitle = $('#alerts-title');
 const $alertsBody = $('#alerts-body');
 
-window.show_alert = function (title, body) {
+window.show_alert = function (title, body, closeFn) {
 	$alertsTitle.text(title);
 	$alertsBody.text(body);
 	$alertsBox.modal('show');
+	
+	$alertsBox.on('hidden.bs.modal', function () {
+		if (typeof closeFn !== "undefined") {
+			closeFn();
+		}
+	});
 };
